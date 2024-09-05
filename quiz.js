1634a6a24aa7848fbabc421c1e324fb951560848
Ryan Davis <itsryzahd@gmail.com>
2024-09-05 16:19:34 +0000

    feat: fixed questions not appearing

--- quiz.js
+++ quiz.js
@@ -41,40 +41,36 @@ const MAX_QUESTIONS = 3;
 function startGame() {
     questionCounter = 0;
     score = 0;
-    availableQuestions = [...questions];
-    console.log(availableQuestions);
+    availableQuesions = [...questions];
     getNewQuestion();
 };
 
 function getNewQuestion() {
-
-
-    if(availableQuestions.length === 0){
+    if (availableQuesions.length === 0 || questionCounter >= MAX_QUESTIONS) {
         //go to the end page
-        return window.location.assign('end.html');
+        return window.location.assign('/end.html');
     }
     questionCounter++;
-    let questionIndex = Math.floor(Math.random() * availableQuestions.length);
-    currentQuestion = availableQuestions[questionIndex];
+    const questionIndex = Math.floor(Math.random() * availableQuesions.length);
+    currentQuestion = availableQuesions[questionIndex];
     question.innerText = currentQuestion.question;
 
-    choices.forEach(choice => {
-        let number = choice.dataset["number"];
-        choice.innerText = currentQuestion["choice" + number];
+    choices.forEach((choice) => {
+        const number = choice.dataset['number'];
+        choice.innerText = currentQuestion['choice' + number];
     });
 
-    availableQuestions.splice(questionIndex, 1);
-
+    availableQuesions.splice(questionIndex, 1);
     acceptingAnswers = true;
 };
 
-choices.forEach(choice => {
-    choice.addEventListener("click", e => {
-        if(!acceptingAnswers) return;
+choices.forEach((choice) => {
+    choice.addEventListener('click', (e) => {
+        if (!acceptingAnswers) return;
 
         acceptingAnswers = false;
         const selectedChoice = e.target;
-        const selectedAnswer = selectedChoice.dataset["number"];
+        const selectedAnswer = selectedChoice.dataset['number'];
         getNewQuestion();
     });
 });
