1c0c637d42df24e68db3c61c702560a874c5af7b
Ryan Davis <itsryzahd@gmail.com>
2024-09-05 16:31:56 +0000

    feat: added js code for correct answers to change to green and incorrect answers to change to red

--- quiz.js
+++ quiz.js
@@ -72,13 +72,16 @@ choices.forEach((choice) => {
         const selectedChoice = e.target;
         const selectedAnswer = selectedChoice.dataset['number'];
 
-        const classToApply = 'incorrect';
-        if (selectedAnswer == currentQuestion.answer) {
-            classToApply = 'correct';
-        }
+        const classToApply = 
+            selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
 
-        getNewQuestion();
+        selectedChoice.parentElement.classList.add(classToApply);
+
+        setTimeout(() => {
+          selectedChoice.parentElement.classList.remove(classToApply);
+          getNewQuestion();
+        }, 1000);
+      });
     });
-});
 
 startGame();
